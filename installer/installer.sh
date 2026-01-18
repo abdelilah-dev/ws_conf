b6244057f8e2e15c61a6a52169ae49c766211f72
abdelilah-dev <contactme.abdelilah@gmail.com>
2026-01-18 17:10:56 +0100

    updated

--- installer/installer.sh
+++ installer/installer.sh
@@ -2,20 +2,13 @@
 
 rm -rf ~/tt
 
-git clone https://github.com/abdelilah-dev/ws_conf.git ~/tt
-make -C ~/tt
+git clone https://github.com/abdelilah-dev/ws_conf.git ~/ws_conf
+make -C ~/ws_conf
 
 if ! grep -q "ws_conf/mbin" ~/.zshrc; then
     echo "ws_conf/mbin" >> ~/.zshrc
 fi
 
-# if [ cat ~/.zshrc | grep "usrbanner" | wc -l ] -eq 0; then
-#     echo "usrbanner" >> ~/.zshrc
-# fi
-
-# if [ cat ~/.zshrc | grep "rctmeme" | wc -l] -eq 0; then
-#     echo "rctmeme" >> ~/.zshrc
-# fi
 if ! grep -q "usrbanner" ~/.zshrc; then
     echo "usrbanner" >> ~/.zshrc
 fi
