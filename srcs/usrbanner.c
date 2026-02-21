0834ac43a5514564708476aa2063a67c1ed8a389
abdelilah-dev <contactme.abdelilah@gmail.com>
2026-02-21 00:12:42 +0000

    updated

--- srcs/usrbanner.c
+++ srcs/usrbanner.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    printf("%s\n", "\x1B[33m");
+    printf("%s\n", "\x1B[36m");
     printf("        ░█▀█░█▄█░▀█▀░█▀▀░█▀▀░█▀█\n"); fflush(stdout); usleep(20000);
     printf("        ░█▀█░█░█░░█░░▀▀█░▀▀█░█▀█\n"); fflush(stdout); usleep(20000);
     printf("        ░▀░▀░▀░▀░▀▀▀░▀▀▀░▀▀▀░▀░▀\n\n"); fflush(stdout); usleep(20000);
